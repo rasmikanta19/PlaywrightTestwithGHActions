9ce05633ba032cd1ba04b91bdaab830d52a710f1
rasmi <rasmi.kanta19@gmail.com>
2026-05-12 10:20:36 +0530

    Enhance Travel Login test with additional logging and URL validation

--- tests/TravelLogin.spec.js
+++ tests/TravelLogin.spec.js
@@ -16,15 +16,23 @@ test.describe('Travel Login', () => {
 
     // Click the "Get started" link
     await expect(page.getByRole('button', { name: 'Sign On',exact: true })).toBeVisible();
+    console.log("Sign On button is visible");
     await page.locator(username).fill('tycUAT2_88664');
     await page.locator(password).fill('Test@1234');
+    console.log("Entered username and password");
     await page.locator(signInButton).click();
-
+    console.log("Clicked on Sign On button");
+    console.log("Waiting for MMB Icon to be visible");
     await page.waitForSelector(`[data-testid='${MMBIcon}']`, { state: 'visible' });
-
+    await expect(page).toHaveURL(/travel\.citi\.com/);
+    console.log("Navigated to Travel page successfully");
+    console.log("MMB Icon is visible");
     await expect(page.getByTestId(MMBIcon)).toBeVisible();
     await page.getByTestId(MMBIcon).click();
+    console.log("Clicked on MMB Icon");
+    await page.close();
     // Expect the URL to contain /docs/intro
+
   
   });
 
